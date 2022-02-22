d58a5d3f4c14d607ab382e2f15017085c7225230
toth julia <t.julia982@gmai.com>
2022-02-22 09:43:03 +0100

    Kezdes

---
+++ App.java
@@ -0,0 +1,32 @@
+/*
+* File: App.java
+* Author: Tóth Júlia
+* Copyright: 2022, Tóth Júlia
+* Group: Szoft 1/n
+* Date: 2022-02-22
+* Github: https://github.com/haribomaci/
+* Licenc: GNU GPL
+ */
+
+import java.util.Scanner;
+public class App {
+	
+	public static void main (String[] args) {
+			System.out.println("Toth Julia, 2022-02-16, Szoft 1/n");
+			System.out.println("Feladat: 0349");
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.println("Kerem az epulet magassagat: ");
+			double magassag = scan.nextDouble();
+			System.out.println("Kerem a szoget: ");
+			double szog = scan.nextDouble();
+			
+			
+			double tavolsag = magassag / szog;
+			System.out.println("A Tavolsag az epulet es a szemely kozott: " + tavolsag);
+			
+			
+		
+	}
+}
+
